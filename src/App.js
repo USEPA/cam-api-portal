533c072f076b89621047ad5ba97ccc4ca64331d8
Johnathan Tafoya <tafoya.johnathan@epa.gov>
2022-01-25 13:42:50 -0500

    Add uswds and react-uswds to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
+import "uswds/dist/css/uswds.css";
+import "@trussworks/react-uswds/lib/index.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello CAM-API-Portal
-        </p>
+        <p>Hello CAM-API-Portal</p>
       </header>
     </div>
   );
