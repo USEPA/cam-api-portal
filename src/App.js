3f9400ab8883671f92172025ca34f08854d65f8f
sgillEPA <gill.sandeep@epa.gov>
2022-01-27 15:52:41 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "uswds/dist/css/uswds.css";
 import "@trussworks/react-uswds/lib/index.css";
+import { GovBanner } from "@trussworks/react-uswds";
 
 function App() {
   return (
     <div className="App">
+      <GovBanner />
       <header className="App-header">
         <p>Hello CAM-API-Portal</p>
       </header>
