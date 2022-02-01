e3ca3827e1d3be8ab154f7f0f8e4727e09fe3dda
Mike Heese <heese.michael@epa.gov>
2022-02-01 10:45:11 -0500

    Added mock for Not Found page

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,22 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
 import { HomePage } from "./components/HomePage";
+import { NotFoundPage } from "./components/NotFoundPage";
 
 jest.mock("./components/HomePage");
+jest.mock("./components/NotFoundPage");
 
 describe("test the router function", () => {
   it("returns a 404 when a bad page is requested", () => {
+    NotFoundPage.mockImplementation(() => <div>Not Found</div>);
+
     render(
       <MemoryRouter initialEntries={["/not-found"]}>
         <App />
       </MemoryRouter>
     );
 
-    expect(screen.getByText(/404/i)).toBeInTheDocument();
+    expect(screen.getByText(/Not Found/i)).toBeInTheDocument();
   });
 
   it("returns the home page when the home page is requested", () => {
