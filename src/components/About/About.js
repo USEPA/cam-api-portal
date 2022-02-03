b1b1a0d484a1589c0c0752832ce61dde2747d4b7
mibarra <ibarra.michelle@epa.gov>
2022-02-03 15:43:55 -0500

    Updated Button components

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { Button, GridContainer, Grid, Link } from "@trussworks/react-uswds";
+import { Button, GridContainer, Grid } from "@trussworks/react-uswds";
 
 export const About = () => {
   return (
@@ -15,7 +15,13 @@ export const About = () => {
             to build applications or create analyses using a variety of
             platforms.
           </p>
-          <Button type="button">Request an API Key</Button>
+
+          <Button
+            className="usa-button"
+            onClick={() => window.open("https://epa.gov/", "_blank")}
+          >
+            Request an API Key
+          </Button>
         </Grid>
 
         <Grid desktop={{ col: 4 }}>
@@ -30,47 +36,36 @@ export const About = () => {
             The Clean Air Markets Program Data (CAMPD) web application is the
             data publication warehouse for allowance, compliance, emissions and
             facility/unit attributes data collected under EPA’s federal
-            market-based trading programs: Cross-State Air Pollution Rule
-            (CSAPR), CSAPR Update, Revised CSAPR Update, Acid Rain Program
-            (ARP), and other retired programs. CAMPD provides current and
-            historical data using the Clean Air Markets (CAM) API portal.
+            market-based trading programs.
           </p>
 
-          <Button type="button" inverse>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href={
-                "https://campd-dev.app.cloud.gov/resources/related-resources"
-              }
-            >
-              CAMPD Application
-            </Link>
+          <Button
+            className="usa-button"
+            onClick={() =>
+              window.open("https://campd-dev.app.cloud.gov/", "_blank")
+            }
+          >
+            CAMPD Application
           </Button>
         </Grid>
 
         <Grid desktop={{ col: true }} className="text-left">
-          <h2>ECMPS 2.0</h2>
+          <h2>ECMPS 2.0 Beta</h2>
           <p>
             The Emissions Collection and Monitoring Plan System 2.0 Beta is the
             new web-based reporting tool released in beta form to allow industry
             users and the public to begin testing and learning the new
-            application. The initial application, released in December of 2021,
-            begins with the limited functionality of view (no login required)
-            and edit/evaluate (CDX Test login required) of monitoring plan data.
-            Subsequent releases throughout 2022 will add other functionality.
-            Data in the beta is not production data and is only being used for
-            testing purposes.
+            application. Data in the beta is not production data and is only
+            being used for testing purposes.
           </p>
 
-          <Button type="button" inverse>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href={"https://easey-beta.app.cloud.gov/ecmps/"}
-            >
-              ECMPS 2.0 Application
-            </Link>
+          <Button
+            className="usa-button"
+            onClick={() =>
+              window.open("https://easey-beta.app.cloud.gov/ecmps/", "_blank")
+            }
+          >
+            ECMPS 2.0 Application
           </Button>
         </Grid>
       </Grid>
