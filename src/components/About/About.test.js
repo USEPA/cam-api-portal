e6a34f9ca438416e54f047b775b7bfd42609beef
mibarra <ibarra.michelle@epa.gov>
2022-02-07 11:56:52 -0500

    Updated About.test.js with tests for paragraph text.

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -11,21 +11,27 @@ describe("Test the About page", () => {
       </MemoryRouter>
     );
 
-    const linkElement = screen.getByText(/About CAM API/i);
+    const linkElement = screen.getByText(
+      /About CAM API/i &&
+        /These REST APIs provide access to a variety of data the Clean Air Markets Division \(CAMD\) collects to run programs designed to reduce air pollution from power plants. Developers can use these APIs to build applications or create analyses using a variety of platforms./i
+    );
     expect(linkElement).toBeInTheDocument();
 
     const btns = screen.getAllByText("Request an API Key");
     expect(btns).toBeDefined();
   });
 
-  test("renders the CAMDP section", () => {
+  test("renders the CAMPD section", () => {
     render(
       <MemoryRouter>
         <About />
       </MemoryRouter>
     );
 
-    const linkElement = screen.getByText(/Clean Air Markets Program Data/i);
+    const linkElement = screen.getByText(
+      /Clean Air Markets Program Data/i &&
+        /The Clean Air Markets Program Data \(CAMPD\) web application is the data publication warehouse for allowance, compliance, emissions and facility\/unit attributes data collected under EPA’s federal market-based trading programs./i
+    );
     expect(linkElement).toBeInTheDocument();
 
     const btns = screen.getAllByText("CAMPD Application");
@@ -39,7 +45,10 @@ describe("Test the About page", () => {
       </MemoryRouter>
     );
 
-    const linkElement = screen.getByText(/ECMPS 2.0 Beta/i);
+    const linkElement = screen.getByText(
+      /ECMPS 2.0 Beta/i &&
+        /The Emissions Collection and Monitoring Plan System 2.0 Beta is the new web-based reporting tool released in beta form to allow industry users and the public to begin testing and learning the new application. Data in the beta is not production data and is only being used for testing purposes./i
+    );
     expect(linkElement).toBeInTheDocument();
 
     const btns = screen.getAllByText("ECMPS 2.0 Application");
