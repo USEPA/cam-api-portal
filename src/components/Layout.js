e9649cdf41efbfbc6cbe964dea536093ee13d068
Johnathan Tafoya <tafoya.johnathan@epa.gov>
2022-02-10 14:50:40 -0500

    Moved SubHeader to separate branch

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { SubHeader } from "./SubHeader/SubHeader";
 
 export const Layout = () => {
   return (
     <div>
-      <SubHeader />
       <main>
         <Outlet />
       </main>
