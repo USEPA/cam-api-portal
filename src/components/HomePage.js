65393ebf832a00a944100dad14803e2936b033cf
mibarra <ibarra.michelle@epa.gov>
2022-02-15 15:52:00 -0500

    Updated HomePage to include APIDocCard component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,19 @@
+import { CardGroup, Grid, GridContainer } from "@trussworks/react-uswds";
 import React from "react";
 
 import { About } from "./About/About.js";
+import { APIDocsCard } from "./APIDocsCard.js";
 
 export const HomePage = () => {
-  return <About />;
+  return (
+    <GridContainer>
+      <Grid row className="padding-3">
+        <About />
+      </Grid>
+
+      <Grid row className="padding-3">
+        <APIDocsCard />
+      </Grid>
+    </GridContainer>
+  );
 };
