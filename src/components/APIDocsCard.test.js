915002eb65d7d8a82f695c03b6fa1af9c3a7eac3
mibarra <ibarra.michelle@epa.gov>
2022-02-28 11:50:52 -0500

    Updated tests - identifer, test to render card, remove MemoryRouter

--- src/components/APIDocsCard.test.js
+++ src/components/APIDocsCard.test.js
@@ -6,39 +6,24 @@ import { APIDocsCard } from "./APIDocsCard";
 
 describe("Test the API Docs Card", () => {
   test("renders the card header text", () => {
-    render(
-      <MemoryRouter>
-        <APIDocsCard />
-      </MemoryRouter>
-    );
-    const linkElement = screen.getByText(
-      /API Documentation/i &&
-        /Lorem ipsum dolor sit amet, consectetur adipiscing elit./i
-    );
+    render(<APIDocsCard />);
+    const linkElement = screen.getByText(/API Documentation/i);
 
     expect(linkElement).toBeInTheDocument();
   });
 
   test("renders the Documentation link text", () => {
-    render(
-      <MemoryRouter>
-        <APIDocsCard />
-      </MemoryRouter>
-    );
-    const linkElement = screen.getByRole("link");
+    render(<APIDocsCard />);
+    const APIdocParagraphElement = screen.getByRole("link");
 
-    expect(linkElement).toHaveTextContent("Documentation");
+    expect(APIdocParagraphElement).toHaveTextContent("Documentation");
   });
 });
 
 expect.extend(toHaveNoViolations);
 
 it("should pass axe accessibility tests", async () => {
-  const { container } = render(
-    <MemoryRouter>
-      <APIDocsCard />
-    </MemoryRouter>
-  );
+  const { container } = render(<APIDocsCard />);
   const results = await axe(container);
 
   expect(results).toHaveNoViolations();
