d6485d4f7d4912ee050804cc01b635dc4a630fb7
sgillEPA <gill.sandeep@epa.gov>
2022-03-09 09:42:44 -0500

    Pushing API Docs left aligned (placeholder) text

--- src/components/APIDocs/APIDocs.js
+++ src/components/APIDocs/APIDocs.js
@@ -3,8 +3,8 @@ import React from "react";
 export const APIDocs = () => {
   return (
     <div>
-      <h1>APIDocs</h1>
-      <p>Lorem ipsum</p>
+      <h1 style={{ textAlign: "left" }}>APIDocs</h1>
+      <p style={{ textAlign: "left" }}>Explore the CAM API Documentation</p>
     </div>
   );
 };
