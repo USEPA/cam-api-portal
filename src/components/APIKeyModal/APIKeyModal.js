e14981c13a1f2f69ddc677bd0b0fdbdd4250f361
kstetsyuk <74557552+kstetsyuk@users.noreply.github.com>
2022-03-21 14:50:05 -0400

    API Key Modal components and test (#99)
    * API Key Modal components and test
    * Add in API request form and .env for creator key

---
+++ src/components/APIKeyModal/APIKeyModal.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Modal,
+  ModalHeading,
+  ModalToggleButton,
+} from "@trussworks/react-uswds";
+
+export const APIKeyModal = () => {
+  const buttonText = "Request an API Key";
+  const modalHeading = "Sign Up for an API Key";
+
+  const modalRef = React.useRef(false);
+
+  const onToggle = () => {
+    modalRef.current = !modalRef.current;
+  };
+
+  return (
+    <div>
+      <ModalToggleButton
+        className="usa-button"
+        modalRef={modalRef}
+        opener={onToggle}
+      >
+        {buttonText}
+      </ModalToggleButton>
+      <Modal
+        ref={modalRef}
+        id="modal-component"
+        aria-labelledby="modal-heading"
+        aria-describedby="modal-decription"
+      >
+        <ModalHeading id="modal-heading">{modalHeading}</ModalHeading>
+        <div id="apidatagov_signup">Loading signup form...</div>
+      </Modal>
+    </div>
+  );
+};
