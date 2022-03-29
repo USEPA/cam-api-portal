cade67bfef5ba1a27a52632b2d4959a341da4ef6
TNGUYE02 <TNGUYE02@LZ27TNGUYE02>
2022-03-29 10:10:53 -0400

    Change jest timeout to 80000

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -1,29 +1,20 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
 import { About } from "./About";
 import { axe, toHaveNoViolations } from "jest-axe";
 
 window.open = jest.fn(); // mock window.open for testing the buttons
-jest.setTimeout(50000);
+jest.setTimeout(80000);
 
 describe("Test the About page", () => {
   test("renders the About section", () => {
-    render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    render(<About />);
 
     expect(screen.getByText(/About CAM API/i)).toBeInTheDocument();
   });
 
   test("renders the CAMPD section", () => {
-    render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    render(<About />);
 
     expect(
       screen.getByText(/Clean Air Markets Program Data/i)
@@ -31,22 +22,14 @@ describe("Test the About page", () => {
   });
 
   test("renders the ECMPS 2.0 section", () => {
-    render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    render(<About />);
 
     expect(screen.getByText(/ECMPS 2.0 Beta/i)).toBeInTheDocument();
   });
 
   test("renders the About section to test buttons", () => {
     window.open.mockClear();
-    const query = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    const query = render(<About />);
 
     const { container, getByText } = query;
 
@@ -62,22 +45,14 @@ describe("Test the About page", () => {
   });
 
   it("should render the What's New box", () => {
-    render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    render(<About />);
     expect(screen.getByText(/What's New/i)).toBeInTheDocument();
   });
 
   expect.extend(toHaveNoViolations);
 
   it("should pass axe accessibility tests", async () => {
-    const { container } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>
-    );
+    const { container } = render(<About />);
     const results = await axe(container);
 
     expect(results).toHaveNoViolations();
