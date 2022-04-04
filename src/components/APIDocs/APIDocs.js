654b5722d546f2864b1e9643b7b1a3c84ce9236b
sgillEPA <gill.sandeep@epa.gov>
2022-04-04 14:15:31 -0400

    API Docs Cards Grid

--- src/components/APIDocs/APIDocs.js
+++ src/components/APIDocs/APIDocs.js
@@ -1,10 +1,30 @@
+import { Grid, GridContainer } from "@trussworks/react-uswds";
 import React from "react";
+import { AccountAPICard } from "./AccountAPICard.js";
+import { EmissionsAPICard } from "./EmissionsAPICard.js";
+import { FacilitiesAPICard } from "./FacilitiesAPICard.js";
 
 export const APIDocs = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: "left" }}>APIDocs</h1>
-      <p style={{ textAlign: "left" }}>Explore the CAM API Documentation</p>
-    </div>
+    <GridContainer>
+      <Grid row gap>
+        <Grid desktop={{ col: 8 }} className="text-left">
+          <h1>APIDocs</h1>
+          <p>Explore the CAM API Documentation</p>
+        </Grid>
+      </Grid>
+
+      <Grid row gap>
+        <Grid desktop={{ col: true }} className="text-left">
+          <AccountAPICard />
+        </Grid>
+        <Grid desktop={{ col: true }} className="text-left">
+          <FacilitiesAPICard />
+        </Grid>
+        <Grid desktop={{ col: true }} className="text-left">
+          <EmissionsAPICard></EmissionsAPICard>
+        </Grid>
+      </Grid>
+    </GridContainer>
   );
 };
