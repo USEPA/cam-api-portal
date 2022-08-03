cde1c591aaf176c15a4010396d2fd3029af79323
Johnathan Tafoya <tafoya.johnathan@epa.gov>
2022-08-03 11:17:26 -0400

    Add request api key button back and fix styling

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link as USWDSLink } from "@trussworks/react-uswds";
+import { Link as USWDSLink, Button } from "@trussworks/react-uswds";
 import { Link as RouterLink } from "react-router-dom";
 import { constants } from "../../helpers/constants";
 
@@ -23,6 +23,22 @@ export const SubHeader = () => {
             </li>
           );
         })}
+        {/* Padding 0 to make the button stay within the header as the same size of the other links and margin-left: auto to shift the button to right adjust */}
+        <li
+          key="signup"
+          className="usa-button"
+          style={{ padding: 0, marginLeft: "auto" }}
+        >
+          <RouterLink to="/api-key-signup" rel="noopener noreferrer">
+            <Button
+              // Have to manually color the button text white, since it inherits the gray from the usa-nav header class
+              style={{ color: "#fff" }}
+              type="button"
+            >
+              Request an API Key
+            </Button>
+          </RouterLink>
+        </li>
       </ul>
     </>
   );
