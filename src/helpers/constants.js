283fb9585b42f7ea79483306ce661ae80950a1e0
Johnathan Tafoya <tafoya.johnathan@epa.gov>
2022-08-03 10:45:56 -0400

    Update constansts for prod

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -3,35 +3,35 @@ export const constants = {
   swaggerPages: [
     {
       name: "account-mgmt",
-      url: "https://api.epa.gov/easey/dev/account-mgmt/swagger-json",
+      url: "https://api.epa.gov/easey/account-mgmt/swagger-json",
       title: "Account Management",
       description:
         "Account management API endpoints for account information, allowance holdings, transactions, and compliance",
     },
     {
       name: "facilities-mgmt",
-      url: "https://api.epa.gov/easey/dev/facilities-mgmt/swagger-json",
+      url: "https://api.epa.gov/easey/facilities-mgmt/swagger-json",
       title: "Facilities Management",
       description:
         "Facility management API endpoints for power sector facilities and their attributes (e.g. units, stacks, and owners)",
     },
     {
       name: "emissions-mgmt",
-      url: "https://api.epa.gov/easey/dev/emissions-mgmt/swagger-json",
+      url: "https://api.epa.gov/easey/emissions-mgmt/swagger-json",
       title: "Emissions Management",
       description:
         "Emissions management API endpoints for apportioned emissions data (e.g. hourly, daily, monthly, annual, and ozone season)",
     },
     {
       name: "master-data-mgmt",
-      url: "https://api.epa.gov/easey/dev/master-data-mgmt/swagger-json",
+      url: "https://api.epa.gov/easey/master-data-mgmt/swagger-json",
       title: "Master Data Management",
       description:
         "Master data management API endpoints for lookup codes and data relationships",
     },
     {
       name: "streaming-services",
-      url: "https://api.epa.gov/easey/dev/streaming-services/swagger-json",
+      url: "https://api.epa.gov/easey/streaming-services/swagger-json",
       title: "Streaming Services Management",
       description:
         "Streaming services API contains endpoints to stream account, allowance, facilities, and emissions data",
