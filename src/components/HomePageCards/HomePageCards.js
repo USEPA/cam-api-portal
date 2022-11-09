ba716ce8a6f427cf8c4e0c3603df599de7f2e568
sgillEPA <80411535+sgillEPA@users.noreply.github.com>
2022-11-09 16:54:52 -0500

    Frequent (#170)
    #167 Change FAQ to Frequent Questions

--- src/components/HomePageCards/HomePageCards.js
+++ src/components/HomePageCards/HomePageCards.js
@@ -43,8 +43,8 @@ export const HomePageCards = () => {
 
           <CardBody>
             <p>
-              Links to Overview, Related Resources, FAQs, Release Notes and
-              Contact Us
+              Links to Overview, Related Resources, Frequent Questions, Release
+              Notes and Contact Us
             </p>
           </CardBody>
 
