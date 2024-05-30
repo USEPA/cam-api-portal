52b511ab6a28faa8d71c12147c9aebf4a2138c60
Johnathan Tafoya <tafoya.johnathan@epa.gov>
2024-05-30 09:46:11 -0400

    hotfix to fix test

--- src/components/APIDocs/APIDocs.test.js
+++ src/components/APIDocs/APIDocs.test.js
@@ -5,8 +5,12 @@ import { axe, toHaveNoViolations } from "jest-axe";
 import { MemoryRouter } from "react-router-dom";
 
 jest.setTimeout(30000);
+window.scrollTo = jest.fn();
 
 describe("test the APIDocs page", () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
   it("renders the main content", () => {
     render(
       <MemoryRouter>
